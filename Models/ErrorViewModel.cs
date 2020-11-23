35a2efb485ba0a4954e73dc54f39609e24d70cfc
Eric <ehill111@comcast.net>
2020-11-23 16:29:20 -0500

    First

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewTrashCollector.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
