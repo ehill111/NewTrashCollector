3014859558da596d040287034f0785250a08323b
Eric <ehill111@comcast.net>
2020-11-24 20:17:04 -0500

    "Created Customer and Employee models."

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewTrashCollector.Models
+{
+    public class Employee
+    {
+    }
+}
